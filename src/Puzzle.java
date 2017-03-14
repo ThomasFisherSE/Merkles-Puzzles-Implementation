bb9edd78c065bcc7c20f21e306a15a52dce0417d
Thomas Fisher <thomas.fisher1@ntlworld.com>
2017-03-14 10:57:41 +0000

    Added DES Class
    Added DES class for encryption and decryption using DES as well as
    generation of DES keys.

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -1,12 +1,30 @@
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
 public class Puzzle {
 	private int m_number;
 	private SecretKey m_key;
-	private byte[] byteArray;
+	private byte[] m_encryptedPuzzle;
+	private String m_cryptogram;
+	private DES m_encryptor;
 	
-	public Puzzle(int number) {
+	public Puzzle(int number) throws Exception {
 		m_number = number;
+		
+		m_encryptor = new DES();
+		
+		m_key = m_encryptor.generateRandomKey(64);
+	}
+	
+	public byte[] encrypt() throws Exception {
+		DES desEncryptor = new DES();
+		
+		desEncryptor.encrypt(m_cryptogram, m_key);
+		
+		return m_encryptedPuzzle;
 		
 	}
 }
--- src/PuzzleGenerator.java
+++ src/PuzzleGenerator.java
@@ -1,9 +1,10 @@
+import java.security.NoSuchAlgorithmException;
 
 public class PuzzleGenerator {
 	
 	private Puzzle[] m_puzzles = new Puzzle[1023];
 	
-	public void generatePuzzles() {
+	public void generatePuzzles() throws NoSuchAlgorithmException {
 		for (int puzzleNo = 0; puzzleNo < m_puzzles.length; puzzleNo++) {
 			m_puzzles[puzzleNo] = new Puzzle(puzzleNo);
 		}
