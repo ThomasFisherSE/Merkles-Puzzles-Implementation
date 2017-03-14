a148494c20e8ba418d42397f0341099dabfee023
James Parry-Turner <jamesparryturner@gmail.com>
2017-03-14 21:29:44 +0000

    Added Main
    Added a Main class so we can divide it away from PuzzleGenerator to make
    it easy to develop. Then just changed the methods and vars from static
    to normal.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main { 
+       public static void main(String [] args) throws Exception {
+           
+            PuzzleGenerator gen = new PuzzleGenerator();
+            gen.generatePuzzles();
+           
+       }
+}
--- src/PuzzleGenerator.java
+++ src/PuzzleGenerator.java
@@ -3,23 +3,19 @@ import java.security.NoSuchAlgorithmException;
 
 public class PuzzleGenerator {
 	
-	private static Puzzle[] m_puzzles = new Puzzle[1023];
-	private static final String OUTPUT_FILE = "puzzles.txt";
+	private Puzzle[] m_puzzles = new Puzzle[1023];
+	private final String OUTPUT_FILE = "puzzles.txt";
 	
-	public static void generatePuzzles() throws Exception {
+	public void generatePuzzles() throws Exception {
 		for (int puzzleNo = 0; puzzleNo < m_puzzles.length; puzzleNo++) {
 			m_puzzles[puzzleNo] = new Puzzle(puzzleNo);
 			writeToFile(m_puzzles[puzzleNo]);
 		}
 	}
 		
-	public static void writeToFile(Puzzle puzzle) throws Exception {
+	public void writeToFile(Puzzle puzzle) throws Exception {
 		PrintWriter out = new PrintWriter(new FileWriter(OUTPUT_FILE, true));
 		out.println(puzzle.toString() + '\n');
 		out.close();
-	}
-	
-	public static void main(String[] args) throws Exception {
-		generatePuzzles();
 	}
 }
