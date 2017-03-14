27a66e8611b045776edbaceae31957193abbb806
Thomas Fisher <thomas.fisher1@ntlworld.com>
2017-03-14 11:15:13 +0000

    Added Encryption Key
    Added a key for DES encryption of a puzzle.

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -7,22 +7,30 @@ import javax.crypto.SecretKey;
 public class Puzzle {
 	private int m_number;
 	private SecretKey m_key;
+	private SecretKey m_encryptionKey;
 	private byte[] m_encryptedPuzzle;
 	private String m_cryptogram;
 	private DES m_encryptor;
+	private byte[] m_unencryptedPuzzle;
 	
 	public Puzzle(int number) throws Exception {
 		m_number = number;
 		
+		byte[] numberInBytes = CryptoLib.smallIntToByteArray(number);
+		
 		m_encryptor = new DES();
 		
 		m_key = m_encryptor.generateRandomKey(64);
+
+		byte[] encodedKey = m_key.getEncoded();
+		
+		m_encryptionKey = CryptoLib.createKey(encodedKey);
 	}
 	
 	public byte[] encrypt() throws Exception {
 		DES desEncryptor = new DES();
 		
-		desEncryptor.encrypt(m_cryptogram, m_key);
+		desEncryptor.encrypt(m_cryptogram, m_encryptionKey);
 		
 		return m_encryptedPuzzle;
 		
--- src/PuzzleGenerator.java
+++ src/PuzzleGenerator.java
@@ -4,7 +4,7 @@ public class PuzzleGenerator {
 	
 	private Puzzle[] m_puzzles = new Puzzle[1023];
 	
-	public void generatePuzzles() throws NoSuchAlgorithmException {
+	public void generatePuzzles() throws Exception {
 		for (int puzzleNo = 0; puzzleNo < m_puzzles.length; puzzleNo++) {
 			m_puzzles[puzzleNo] = new Puzzle(puzzleNo);
 		}
