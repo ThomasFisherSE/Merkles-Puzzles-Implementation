cc2169e15a197db758888a8275c92467b1a946a3
Thomas Fisher <thomas.fisher1@ntlworld.com>
2017-03-21 19:03:37 +0000

    Started implementation of cracking algorithm
    Chooses random puzzle, changes each bit of the key in turn and tries to
    decrypt using it.

--- src/Cracking.java
+++ src/Cracking.java
@@ -3,32 +3,58 @@ import javax.crypto.SecretKey;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Cracking {
 	
-	public void crackPuzzle(Puzzle puzzle, SecretKey key) throws Exception {
-		DES encryptor = new DES();
-		
-		encryptor.decrypt(puzzle.toString(), key);
+	public static void main(String[] args) throws Exception {
+		// Choose random puzzle
+		String puzzle = randomPuzzle();
+		System.out.println("Cracking puzzle: " + puzzle);
+		crack(puzzle);
 	}
 	
-	public Puzzle[] readPuzzleFile(String filepath) throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(filepath));
+	public static void crack(String puzzle) throws Exception {
+		DES encryptor = new DES();
+	
+		byte[] keyBytes = new byte[8];
 		
-		//String[] cryptograms = new String[]
-		
-		String line;
-		
-		while (reader.readLine() != null) {
-			
+		for (int i = 0; i < keyBytes.length; i++) {
+			keyBytes[i] = 0;
 		}
 		
-		return null;
+		for (int i = 0; i < keyBytes.length; i++) {
+			for (int j = 0; j < 8; j++) {
+				keyBytes[i] |= (byte) (1 << j);
+				SecretKey key = CryptoLib.createKey(keyBytes);
+				encryptor.decrypt(puzzle, key);
+			}
+		}
 	}
 	
-	public void crackPuzzles(Puzzle[] puzzles) {
-		for (int i = 0; i < puzzles.length; i++) {
-			//crackPuzzle(puzzles[i], )
+	public static String randomPuzzle() throws IOException {
+		String[] puzzles = readPuzzleFile("puzzles.txt");
+		return puzzles[(int)(Math.random() * puzzles.length)];
+	}
+	
+	public static String[] readPuzzleFile(String filepath) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(filepath));	
+		List<String> lines = new ArrayList<String>();
+        String line = null;
+        
+        while ((line = reader.readLine()) != null) {
+            lines.add(line);
+        }
+        
+        reader.close();
+        
+        return lines.toArray(new String[lines.size()]);
+	}
+	
+	public void crackPuzzles(String[] puzzles) throws Exception {
+		for (String puzzle : puzzles) {
+			crack(puzzle);
 		}
 	}
 }
